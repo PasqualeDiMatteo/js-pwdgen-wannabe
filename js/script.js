cfcb828350e9d596dccb74a40845389de45ed3ee
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-05-25 16:04:31 +0200

    added value 21

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log('JS OK')
+
+// Memorizzo in una variabile il Valore 21
+
+const passwordNumber = '21'
+const passwordPlaceholder = document.getElementById('password');
+console.log(passwordPlaceholder);
