2415ff544c2e99f77462e11c3474ee10d4b926b1
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-05-25 16:16:48 +0200

    Added Password Generator

--- js/script.js
+++ js/script.js
@@ -13,10 +13,15 @@ console.log(firstName);
 
 // Chiedo all'utente il suo cognome e lo salvo;
 
-const lastName = prompt('Inserisci il Cognome', 'Di Matteo')
+const lastName = prompt('Inserisci il Cognome', 'DiMatteo')
 console.log(lastName);
 
 // Chiedo all'utente il suo colore preferito e lo salvo;
 
 const favoriteColor = prompt('Inserisci il tuo colore preferito', 'Blu')
 console.log(favoriteColor);
+
+// Uso tutti gli elementi generando una password e la salvo;
+
+const generatePassword = firstName + lastName + favoriteColor + passwordNumber
+console.log(generatePassword);
